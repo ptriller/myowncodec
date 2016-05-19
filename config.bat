e1c5b40085ff586673d7b13b8579edb350171eb6
ptriller <ptriller@soapwars.de>
2016-05-19 22:29:28 +0200

    more build stuff

--- config.bat
+++ config.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
+setlocal
+PATH=%PATH%;E:/devel/mingw-tdm/bin
 set curr_dir=%cd%
+IF EXIST E:/devel/project/eclipse/myowncodec GOTO IS_THERE
+echo Creating dir
+MKDIR "E:/devel/project/eclipse/myowncodec"
+:IS_THERE
 cd /D "E:/devel/project/eclipse/myowncodec"
 E:\devel\cmake-3.5.2-win32-x86\bin\cmake.exe -D "CMAKE_BUILD_TYPE:STRING=Debug"^
  -D "LibJpegTurbo_INCLUDE_DIR:PATH=E:/devel/libjpeg-turbo-gcc64/include"^
@@ -9,5 +15,7 @@ E:\devel\cmake-3.5.2-win32-x86\bin\cmake.exe -D "CMAKE_BUILD_TYPE:STRING=Debug"^
  -G "Eclipse CDT4 - MinGW Makefiles"^
  -D "CMAKE_ECLIPSE_EXECUTABLE:FILEPATH=E:/devel/eclipse/eclipse.exe"^
  -D "CMAKE_ECLIPSE_VERSION:STRING=4.3 (Kepler)"^
+ -D "JPEG_INCLUDE_DIR:PATH=E:\devel\libjpeg-turbo-gcc64\include"^
+ -D "JPEG_LIBRARY:PATH=E:\devel\libjpeg-turbo-gcc64\lib"^
  "E:/devel/project/myowncodec" 
 cd /D %curr_dir%
